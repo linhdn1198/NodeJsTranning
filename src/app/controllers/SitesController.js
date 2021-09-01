cb62824760c547dc5630a4875e2b29a91523811b
Binh <binh@payrexx.com>
2021-09-01 17:33:45 +0700

    init

---
+++ src/app/controllers/SitesController.js
@@ -0,0 +1,14 @@
+class SitesController {
+
+    // [GET] /
+    index(req, res) {
+        res.render('home');
+    }
+
+    // [GET] /search
+    search(req, res) {
+        res.send('search');
+    }
+}
+
+module.exports = new SitesController;
