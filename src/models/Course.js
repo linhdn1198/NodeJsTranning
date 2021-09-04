5804f696b86dc69a823ab162c5f087860bb2c2dc
Binh <binh@payrexx.com>
2021-09-04 09:36:42 +0700

    Delete course

--- src/models/Course.js
+++ src/models/Course.js
@@ -7,7 +7,7 @@ mongoose.plugin(slug);
 const Course = new Schema(
     {
         name: { type: String, maxlength: 255 },
-        slug: { type: String, slug: 'name' },
+        slug: { type: String, slug: 'name', unique: true },
         description: { type: String, maxlength: 600 },
         image: { type: String, maxlength: 255 },
     },
